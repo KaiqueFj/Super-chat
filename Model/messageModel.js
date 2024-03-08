c62d71bac4ef2ecb329cbfee96f761272ce843ff
KaiqueFj <kaiquelferraz@hotmail.com>
2024-03-08 12:14:07 -0300

    feat: added the feature to retrieve the messages from mongodDB database and also, simutaniously receive and show new messages to the user

--- Model/messageModel.js
+++ Model/messageModel.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  name: {
-    type: String,
-  },
   message: {
     type: String,
-    required: ["must have a message"],
+    required: ['must have a message'],
+  },
+  room: {
+    type: String,
   },
 });
 
-const Message = mongoose.model("Messages", messageSchema);
+const Message = mongoose.model('Messages', messageSchema);
 
 module.exports = Message;
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const http = require("http");
-
-const mongoose = require("mongoose");
-const socketIo = require("socket.io");
-const app = require("./app");
+const http = require('http');
+const mongoose = require('mongoose');
+const socketIo = require('socket.io');
+const app = require('./app');
 const server = http.createServer(app);
+const dotenv = require('dotenv');
+const Message = require('./Model/messageModel');
 const io = socketIo(server);
-const dotenv = require("dotenv");
-const Message = require("./Model/messageModel");
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: './.env' });
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
@@ -15,7 +14,7 @@ server.listen(PORT, () => {
 });
 
 const DB = process.env.DATABASE.replace(
-  "<password>",
+  '<password>',
   process.env.DATABASE_PASSWORD
 );
 
@@ -23,40 +22,48 @@ mongoose
   .connect(DB)
   .then(() => console.log(`DB connection successfully established`));
 
-  
-process.on("uncaughtException", (err) => {
-  console.log("UncaughtException, shutting down...");
+process.on('uncaughtException', (err) => {
+  console.log('UncaughtException, shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
-// IO
-
-//handle chat messages
-io.on("connection", (socket) => {
-  socket.on("send-message", (message, room) => {
-
-    const newMessage = new Message(message)
-    newMessage.save()
-    if (room === "") {
-      socket.broadcast.emit("received-message", message);
-    } else {
-      socket.to(room).emit("received-message", message);
+//handles the chat message
+io.on('connection', (socket) => {
+  socket.on('send-message', async (message) => {
+    try {
+      const newMessage = new Message({
+        message: message.message,
+        room: message.room,
+      });
+      await newMessage.save();
+      if (message.room === '') {
+        socket.broadcast.emit('received-message', message);
+      } else {
+        socket.to(room).emit('received-message', message);
+      }
+    } catch (err) {
+      console.error(err);
     }
-  })
-
-    socket.on("join-room", (room, cb) => {
-      socket.join(room);
-      cb(`Joined ${room}`);
-    });
   });
 
+  socket.on('getUsers', async () => {
+    try {
+      const userMessage = await Message.find();
+      socket.emit('users', userMessage);
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
-//handle disconnections
-io.on("connection", (socket) => {
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  //socket method used to join rooms
+  socket.on('join-room', (room, cb) => {
+    socket.join(room);
+    cb(`Joined ${room}`);
   });
 });
 
-
+//handle disconnections
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {});
+});
