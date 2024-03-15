5c1cd3b4d9e3d84dce987ca7272f04d92a89c765
KaiqueFj <kaiquelferraz@hotmail.com>
2024-03-14 22:35:27 -0300

    feat:created a controller for the features of the chat, and cleaned the code of server.js

---
+++ Controllers/chatController.js
@@ -0,0 +1,42 @@
+const Message = require('../Model/messageModel');
+const AppError = require('../utils/AppError');
+
+exports.chatFeatures = (io) => {
+  io.on('connection', (socket) => {
+    socket.on('send-message', async (message) => {
+      try {
+        const newMessage = new Message({
+          message: message.message,
+          room: message.room,
+        });
+        await newMessage.save();
+
+        message.room === ''
+          ? socket.broadcast.emit('received-message', message)
+          : socket.to(room).emit('received-message', message);
+      } catch (err) {
+        return next(new AppError('Could not send the messages properly'), 400);
+      }
+    });
+
+    socket.on('getUsers', async () => {
+      try {
+        const userMessage = await Message.find();
+        socket.emit('users', userMessage);
+      } catch (err) {
+        return next(new AppError('Could not load the messages properly'), 400);
+      }
+    });
+
+    //socket method used to join rooms
+    socket.on('join-room', (room, cb) => {
+      socket.join(room);
+      cb(`Joined ${room}`);
+    });
+  });
+
+  //handle disconnections
+  io.on('connection', (socket) => {
+    socket.on('disconnect', () => {});
+  });
+};
--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const socketIo = require('socket.io');
 const app = require('./app');
 const server = http.createServer(app);
 const dotenv = require('dotenv');
-const Message = require('./Model/messageModel');
-const AppError = require('./utils/AppError');
 const io = socketIo(server);
+const chatController = require('./Controllers/chatController');
 dotenv.config({ path: './.env' });
 
 const PORT = process.env.PORT || 8000;
@@ -30,40 +29,4 @@ process.on('uncaughtException', (err) => {
 });
 
 //handles the chat message
-io.on('connection', (socket) => {
-  socket.on('send-message', async (message) => {
-    try {
-      const newMessage = new Message({
-        message: message.message,
-        room: message.room,
-      });
-      await newMessage.save();
-
-      message.room === ''
-        ? socket.broadcast.emit('received-message', message)
-        : socket.to(room).emit('received-message', message);
-    } catch (err) {
-      return next(new AppError('Could not send the messages properly'), 400);
-    }
-  });
-
-  socket.on('getUsers', async () => {
-    try {
-      const userMessage = await Message.find();
-      socket.emit('users', userMessage);
-    } catch (err) {
-      return next(new AppError('Could not load the messages properly'), 400);
-    }
-  });
-
-  //socket method used to join rooms
-  socket.on('join-room', (room, cb) => {
-    socket.join(room);
-    cb(`Joined ${room}`);
-  });
-});
-
-//handle disconnections
-io.on('connection', (socket) => {
-  socket.on('disconnect', () => {});
-});
+chatController.chatFeatures(io);
