c0586c15f53466403004b872b9754d8217d140ab
KaiqueFj <kaiquelferraz@hotmail.com>
2024-06-11 13:16:07 -0300

    feat: added a better error handling for the application, that return specific messages errors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser'); // Import bodyParser module
+const globalErrorHandler = require('./Controllers/errorController');
 
 const useRouter = require('./routes/userRoutes');
 const viewRouter = require('./routes/viewRoutes');
@@ -25,5 +25,7 @@ app.use('/styles', (req, res, next) => {
 //Routes
 app.use('/', viewRouter);
 app.use('/api/v1/users', useRouter);
+
+app.use(globalErrorHandler);
 
 module.exports = app;
