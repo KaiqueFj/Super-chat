235ef24f797f55bae31a0da7423468892fc5e338
KaiqueFj <kaiquelferraz@hotmail.com>
2024-06-21 23:22:26 -0300

    feat:added sanitization, security and cors into the application, for the prevent of malicous attacks and inserts into the database

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const globalErrorHandler = require('./Controllers/errorController');
-
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const compression = require('compression');
+const cors = require('cors');
 const useRouter = require('./routes/userRoutes');
 const viewRouter = require('./routes/viewRoutes');
+const globalErrorHandler = require('./Controllers/errorController');
+
 const app = express();
+
+//implement cors
+app.use(cors());
+app.options('*', cors());
 
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+// Data sanitization against XSS
+app.use(xss());
+app.use(compression());
+
+// Data sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+// Limit requests from the same api
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this Ip, please try again in an hour',
+});
+app.use('/api', limiter);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
